428945994935855c6af3ee3bb525a28e7638c735
Steve Davey <double.happiness@outlook.com>
2025-09-10 17:50:04 +0100

    Chore: import order

--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -1,5 +1,5 @@
-import { Application } from 'express';
 import axios from 'axios';
+import { Application } from 'express';
 
 export default function (app: Application): void {
   app.get('/', async (req, res) => {
